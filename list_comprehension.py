b8844e165724e7422a37ebf8721f56e4f42ecf55
Jack Owens <40874726+rjackowens@users.noreply.github.com>
2020-12-03 14:48:51 -0600

    Create list_comprehension.py

---
+++ list_comprehension.py
@@ -0,0 +1,10 @@
+### SYNTAX ###
+new_list = [expression for member in iterable]
+
+# long way
+items = []
+for num in range(5):
+    items.append(num)
+
+# list comprehension
+items = [(num) for num in range(5)]
